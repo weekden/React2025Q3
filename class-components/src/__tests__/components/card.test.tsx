de781a3e8d53f55ccfb9f70d1edf03e975f3212f
DenisNedelko <dnsndlk@gmail.com>
2025-07-19 10:53:56 +0300

    test: add unit test for Card component 2025-07-19 10:53:56

---
+++ class-components/src/__tests__/components/card.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';
+import Card from '../../components/card/Card';
+import type { CardProps } from '../../types/card';
+
+describe('Card component', () => {
+  const cardDefaultprops: CardProps = {
+    name: 'Anche',
+    race: 'Gerudo',
+    description: 'Anche is a character in Breath of the Wild.',
+  };
+  it('should render card element', () => {
+    render(<Card {...cardDefaultprops} />);
+    expect(screen.getByText('Anche')).toBeInTheDocument();
+  });
+
+  it('should display the unknow race if no race is specified', () => {
+    render(
+      <Card
+        name={cardDefaultprops.name}
+        description={cardDefaultprops.description}
+      />
+    );
+    expect(screen.getByText('Unknow race')).toBeInTheDocument();
+  });
+});
