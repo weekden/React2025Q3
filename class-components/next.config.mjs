2d0b27b704ca37d3bb71d9342cf74f85c0590227
DenisNedelko <dnsndlk@gmail.com>
2025-08-17 20:03:43 +0300

    chore: update next config 2025-08-17 20:03:43

--- class-components/next.config.mjs
+++ class-components/next.config.mjs
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  distDir: './dist',
   reactStrictMode: true,
   images: {
     unoptimized: true,
